ba7a2850677c8ec57f9f48e37948dab062899b28
vitalik1970 <ytcenko1970@gmail.com>
2024-12-20 23:32:13 +0200

    fix

--- app/dashboard/bid/page.js
+++ app/dashboard/bid/page.js
@@ -7,6 +7,7 @@ export default function Bid() {
   const [description, setDescription] = useState('');
   const [responseMessage, setResponseMessage] = useState('');
   const [showResponseMessage, setShowResponseMessage] = useState(false);
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -40,7 +41,7 @@ export default function Bid() {
        <p>Оставьте свои пожелания и  контакты, и мы свяжемся с вами для консультации по выбору готового сайта, а также расскажем о возможностях его настройки под ваш бизнес!</p>
        <p>Или просто оставте ваш отзыв о сайте и наших услугах, ваше мнение очень важно для нас!</p>
      {/* <p>Извините, мы ещё работам над создание API для обработки данных на сервере, поэтому напишите нам пожалуйста на электронную почту   <a href="mailto:ytcenko1970@gmail.com">ytcenko1970@gmail.com</a> или позвоните по телефону<a href= 'tel:+380953945157' >+38 095 394 51 57</a>, или через социальные сети.<br/> Просим извинения за неудобства.</p> */}
-    <form onSubmit={handleSubmit} className={styles.feedbackform }  netlify data-netlify="true">
+    <form onSubmit={handleSubmit} className={styles.feedbackform }  name="contact" method="POST" data-netlify="true">
       <textarea
         value={description}
         onChange={(e) => setDescription(e.target.value)}
